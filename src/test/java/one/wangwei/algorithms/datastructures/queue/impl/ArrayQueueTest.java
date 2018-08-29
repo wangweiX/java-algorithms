539eb37f705d3bcb62825b72e17f7a9578f8820a
wangwei <w7years@gmail.com>
2018-08-29 22:01:41 +0800

    代码迁移到新工程

---
+++ src/test/java/one/wangwei/algorithms/datastructures/queue/impl/ArrayQueueTest.java
@@ -0,0 +1,36 @@
+package one.wangwei.algorithms.datastructures.queue.impl;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ArrayQueueTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @Test
+    public void testOffer() {
+        System.out.println(5 % 3);
+    }
+
+    @Test
+    public void testPoll() {
+    }
+
+    @Test
+    public void testPeek() {
+    }
+
+    @Test
+    public void testRemove() {
+    }
+
+    @Test
+    public void testClear() {
+    }
+
+    @Test
+    public void testSize() {
+    }
+}
