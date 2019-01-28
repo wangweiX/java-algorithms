2f8603c50aee723de24bd9660b34bc7352778bde
wangwei <w7years@gmail.com>
2019-01-28 09:32:47 +0800

    add lib

---
+++ src/test/java/one/wangwei/algorithms/datastructures/stack/impl/Test.java
@@ -0,0 +1,12 @@
+package one.wangwei.algorithms.datastructures.stack.impl;
+
+/**
+ * @author https://wangwei.one
+ * @date 2019/1/27
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
